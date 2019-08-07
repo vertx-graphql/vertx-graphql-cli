1fd656c78d7ead04685ee26577fc983be58d892b
Kai Wedekind <wedekind.kai@googlemail.com>
2019-08-07 15:28:10 +0200

    Add more commands

---
+++ bin/index.js
@@ -0,0 +1,172 @@
+#!/usr/bin/env node
+
+// Check node version before requiring/doing anything else
+// The user may be on a very old node version
+
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const slash = require('slash')
+const minimist = require('minimist')
+const program = require('commander')
+const didYouMean = require('didyoumean')
+
+const environment = require('../lib/environment')
+const pkg = require('../package.json')
+
+environment.checkNodeVersion(pkg.engines.node, 'vertx-graphql-cli')
+environment.checkRecommendations()
+
+program
+  .version(pkg.version, '-v, --version')
+  .description(pkg.description)
+  .usage('<command> [options]')
+
+program
+  .command('create <app-name>')
+  .description('create a new project powered by vertx-graphql-cli')
+  .option('-d, --default', 'Skip prompts and use default preset')
+  .option('-g, --git [message]', 'Force git initialization with initial commit message')
+  .option('-n, --no-git', 'Skip git initialization')
+  .option('-f, --force', 'Overwrite target directory if it exists')
+  .option('-b, --bare', 'Scaffold project without beginner instructions')
+  .option('--skipGetStarted', 'Skip displaying "Get started" instructions')
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd)
+
+    /*
+    if (minimist(process.argv.slice(3))._.length > 1) {
+      console.log(chalk.yellow('\n Info: You provided more than one argument. The first one will be used as the app\'s name, the rest are ignored.'))
+    }
+    // --git makes commander to default git to true
+    if (process.argv.includes('-g') || process.argv.includes('--git')) {
+      options.forceGit = true
+    }
+    require('../lib/create')(name, options)
+    */
+  })
+
+program
+  .command('serve [entry]')
+  .description('serve a .js or .vue file in development mode with zero config')
+  .option('-o, --open', 'Open browser')
+  .option('-c, --copy', 'Copy local url to clipboard')
+  .option('-p, --port <port>', 'Port used by the server (default: 8080 or next available port)')
+  .action((entry, cmd) => {
+    // loadCommand('serve', '@vue/cli-service-global').serve(entry, cleanArgs(cmd))
+  })
+
+program
+  .command('build [entry]')
+  .description('build a .js or .vue file in production mode with zero config')
+  .option('-t, --target <target>', 'Build target (app | lib | wc | wc-async, default: app)')
+  .option('-n, --name <name>', 'name for lib or web-component mode (default: entry filename)')
+  .option('-d, --dest <dir>', 'output directory (default: dist)')
+  .action((entry, cmd) => {
+    // loadCommand('build', '@vue/cli-service-global').build(entry, cleanArgs(cmd))
+  })
+
+program
+  .command('ui')
+  .description('start and open the vertx-graphql-cli ui')
+  .option('-H, --host <host>', 'Host used for the UI server (default: localhost)')
+  .option('-p, --port <port>', 'Port used for the UI server (by default search for available port)')
+  .option('-D, --dev', 'Run in dev mode')
+  .option('--quiet', `Don't output starting messages`)
+  .option('--headless', `Don't open browser on start and output port`)
+  .action((cmd) => {
+    environment.checkNodeVersion('>=8.6', 'vertx-graphql ui')
+    // require('../lib/ui')(cleanArgs(cmd))
+  })
+
+program
+  .command('info')
+  .description('print debugging information about your environment')
+  .action((cmd) => {
+    console.log(chalk.bold('\nEnvironment Info:'))
+    require('envinfo').run(
+      {
+        System: ['OS', 'CPU'],
+        Binaries: ['Node', 'Yarn', 'npm'],
+        Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
+        npmGlobalPackages: ['vertx-graphql-cli']
+      },
+      {
+        showNotFound: true,
+        duplicates: true,
+        fullTree: true
+      }
+    ).then(console.log)
+  })
+
+// output help information on unknown commands
+program
+  .arguments('<command>')
+  .action((cmd) => {
+    program.outputHelp()
+    console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+    console.log()
+    suggestCommands(cmd)
+  })
+
+// add some useful info on help
+program.on('--help', () => {
+  console.log()
+  console.log(`  Run ${chalk.cyan(`vertx-graphql <command> --help`)} for detailed usage of given command.`)
+  console.log()
+})
+
+program.commands.forEach(c => c.on('--help', () => console.log()))
+
+// enhance common error messages
+const ErrorMessages = require('../lib/error_messages')
+
+ErrorMessages.enhance('missingArgument', argName => {
+  return `Missing required argument ${chalk.yellow(`<${argName}>`)}.`
+})
+
+ErrorMessages.enhance('unknownOption', optionName => {
+  return `Unknown option ${chalk.yellow(optionName)}.`
+})
+
+ErrorMessages.enhance('optionMissingArgument', (option, flag) => {
+  return `Missing required argument for option ${chalk.yellow(option.flags)}` + (
+    flag ? `, got ${chalk.yellow(flag)}` : ``
+  )
+})
+
+program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
+
+function suggestCommands (unknownCommand) {
+  const availableCommands = program.commands.map(cmd => {
+    return cmd._name
+  })
+
+  const suggestion = didYouMean(unknownCommand, availableCommands)
+  if (suggestion) {
+    console.log(`  ` + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`))
+  }
+}
+
+function camelize (str) {
+  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+}
+
+// commander passes the Command object itself as options,
+// extract only actual options into a fresh object.
+function cleanArgs (cmd) {
+  const args = {}
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  return args
+}
